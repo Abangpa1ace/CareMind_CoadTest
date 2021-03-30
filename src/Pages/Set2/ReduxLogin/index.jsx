b22f60bb828974874244a345d0085f7a251e93b4
Abangpa1ace <dyek72731@gmail.com>
2021-03-30 18:51:54 +0900

    Add: 2-3 ReduxLogin Store 세팅중

---
+++ src/Pages/Set2/ReduxLogin/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ReduxLogin = () => {
+  return (
+    <ReduxLoginPage>
+      
+    </ReduxLoginPage>
+  )
+}
+
+const ReduxLoginPage = styled.main`
+
+`;
+
+export default ReduxLogin
